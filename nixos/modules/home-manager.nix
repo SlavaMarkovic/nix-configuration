c51bbfa6944e923cc9f51011c14d5123f82d123f
SlavaMarkovic <markovicslavko666@gmail.com>
2026-01-22 11:17:35 +0200

    Enhance home-manager module with user support
    Refactor home-manager module to include user and additional configurations.

--- nixos/modules/home-manager.nix
+++ nixos/modules/home-manager.nix
@@ -1,4 +1,18 @@
-{ inputs, ... }: {
-  imports = [ inputs.home-manager.nixosModules.default ];
-  home-manager.backupFileExtension = "backup";
+{ inputs, user, ... }: { # Получаем переменную 'user' из specialArgs
+  imports = [
+     # Подключаем модуль HM к системе
+     inputs.home-manager.nixosModules.default
+  ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "backup";
+
+    # ВАЖНО: Передаем inputs и user внутрь home-manager, чтобы они были доступны в home.nix
+    extraSpecialArgs = { inherit inputs user; };
+
+    # ВАЖНО: Явно указываем конфигурацию пользователя
+    users.${user} = import ../../home-manager/home.nix;
+  };
 }
